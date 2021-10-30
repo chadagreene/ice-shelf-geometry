d5bc8108750ac8a37ca9db7d8e7f646cd604e4ae
Chad A. Greene <chad@chadagreene.com>
2021-10-29 18:32:30 -0700

    Update calving_thinning_timeseries.m

--- calving_thinning_timeseries.m
+++ calving_thinning_timeseries.m
@@ -22,3 +22,24 @@ axis tight
 ax(2) = gca; 
 ylabel 'mass change (Gt') 
 linkaxes(ax,'x')
+
+%%
+
+
+figure
+plot(year,A_calving(:,132))
+hold on
+polyfit(year(5:end),A_calving(5:end,132),1)
+ft = polyfit(year(5:end),A_calving(5:end,132),1)
+plot(2005:2100,polyval(ft,2005:2100))
+yline(A_calving(1,132))
+
+figure
+plot(year,M_calving(:,132))
+hold on
+ftm = polyfit(year(5:end),M_calving(5:end,132),1)
+plot(2005:2100,polyval(ftm,2005:2100))
+yline(M_calving(1,132))
+title mass
+
+
