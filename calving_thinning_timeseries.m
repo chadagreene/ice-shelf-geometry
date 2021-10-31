94b362dac5e305028391786aa3cc4c992f3c5853
Chad A. Greene <chad@chadagreene.com>
2021-10-31 14:59:03 -0700

    daily updates

--- calving_thinning_timeseries.m
+++ calving_thinning_timeseries.m
@@ -5,11 +5,28 @@
 load('calving_flux_timeseries.mat') 
 D = load('iceshelves_2008_v2.mat');
 
+% Convert to thousands of square km: 
+A_calving = A_calving*1e-9; 
+
+ronne_mass_per_area = diff(A_calving(:,132))\diff(M_calving(:,132));
+
+year = [year 2021.4]; 
+A_calving = [A_calving;NaN(1,183)];
+A_calving(end,132) = A_calving(end-1,132)-4.320; % Iceberg A-76 https://www.esa.int/ESA_Multimedia/Images/2021/05/Meet_the_world_s_largest_iceberg#.YKTfep3wFKU.link
+A_calving(end,end) = A_calving(end-1,end)-4.320; 
+
+M_calving = [M_calving;NaN(1,183)];
+M_calving(end,132) = M_calving(end-1,132)-4.320*ronne_mass_per_area; 
+M_calving(end,end) = M_calving(end-1,end)-4.320*ronne_mass_per_area; 
+
+M_calving_err = [M_calving_err;M_calving_err(end,:)];
+
+yearc = year(1:end-1)+diff(year)/2; 
 %%
 
 figure
 subplot(2,1,1) 
-plot(year,(A_calving-A_calving(1,:))/1e9)
+plot(year,(A_calving-A_calving(1,:)))
 box off
 axis tight
 ax(1) = gca; 
@@ -25,21 +42,117 @@ linkaxes(ax,'x')
 
 %%
 
+col = hex2rgb({'#50b47b';
+'#b94b75';
+'#7f63b8';
+'#98a441';
+'#ba6437'});
+
+extrapyr = 2021.5:2040; 
+ind = 10;
+figure('pos',[19.00        553.00        560.00        337.00]) 
+figure('pos',[19.00        553.00        794.00        337.00]) 
+
+% Ronne A-38 October 1998, and May 2000 A-43; also May 2021 A-76 4320 km^2 
+ft = polyfit(year(11:end-1),A_calving(11:end-1,132),1);
+tmp = A_calving(:,132) - (A_calving(1,132) +ft(1)*1);
+yr3 = [year(1) 1998.8 year(2:end)]; 
+tmp3 = [tmp(1) 0 tmp(2:end)']; 
+plot(yr3,tmp3,'.-','color',col(1,:),'linew',1,'markersize',10)  
+hold on
+plot(extrapyr,polyval(ft,extrapyr) - (A_calving(1,132) +ft(1)*1)-4.32,':','color',col(1,:),'linew',1)
+text(year(ind),tmp(ind),'Ronne','horiz','left','vert','top','fontsize',8,'color',col(1,:))
+%plot(1998.8,0,'x','color',col(1,:),'linew',1)
+text(1998.8,0,{'Iceberg A-38, 7100 km^2';'Oct 13, 1998';'\it{Larour et al., 2002}'},'vert','bot','horiz','left','fontsize',6,'color',col(1,:))
+
+
+% Ross East: C-19 May 2002 
+ft = polyfit(year(6:end-1),A_calving(6:end-1,134),1); 
+tmp = A_calving(:,134) - (A_calving(3,134) +ft(1)*2/12);
+yr3 = [year(1:5)  year(6:end)]; 
+tmp3 = [tmp(1:5)' tmp(6:end)']; 
+plot(yr3,tmp3,'.-','color',col(2,:),'linew',1,'markersize',10)
+plot(extrapyr,polyval(ft,extrapyr) - (A_calving(3,134) +ft(1)*2/12),':','color',col(2,:),'linew',1)
+text(year(ind),tmp(ind),'Ross East','horiz','left','vert','top','fontsize',8,'color',col(2,:))
+%plot(2002.4,0,'x','color',col(2,:),'linew',1)
+
+% Western Ross: B-15 March 2000 
+ft = polyfit(year(4:end-1),A_calving(4:end-1,135),1);
+tmp = A_calving(:,135)-(A_calving(1,135)+ft(1)*2.5);
+yr3 = [year(1) 2000.18 year(2:end)]; 
+tmp3 = [tmp(1) 0 tmp(2:end)']; 
+plot(yr3,tmp3,'.-','color',col(3,:),'linew',1,'markersize',10)
+plot(extrapyr,polyval(ft,extrapyr)-(A_calving(1,135)+ft(1)*2.5),':','color',col(3,:),'linew',1)
+text(year(ind),tmp(ind),'Ross West','horiz','left','vert','top','fontsize',8,'color',col(3,:))
+%plot(2000.18,0,'x','color',col(3,:),'linew',1)
+% B-9 October 1987, 4540 km^2 keys1990calving
+plot([1987.7 1987.8 year(1)],[0 -4.54 tmp(1)],':.','color',col(3,:),'linew',1,'markersize',10)
+text(1987.7,0,{'Iceberg B-9, 4540 km^2';'Oct 1987';'\it{Keys et al., 1990}'},'vert','bot','horiz','left','fontsize',6,'color',col(3,:))
+
+% Filchner 1986 calving event created A22, A23, and maybe A24 but I'm not sure. 
+ft = polyfit(year(4:end-1),A_calving(4:end-1,48),1);
+tmp = A_calving(:,48)-(A_calving(1,48)-ft(1)*11)-11.5; % 11,500 km^2 calving event in 1986 according to Ferringo1987substantial 
+plot(year,tmp,'.-','color',col(4,:),'linew',1,'markersize',10)
+ft = polyfit(year(11:end-1),tmp(11:end-1),1);
+plot(extrapyr,polyval(ft,extrapyr),':','color',col(4,:),'linew',1)
+text(year(ind),tmp(ind),'Filchner','horiz','left','vert','top','fontsize',8,'color',col(4,:))
+plot([1986 1986.1 year(1)],[0 -11.5 tmp(1)],':.','color',col(4,:),'linew',1,'markersize',10)
+text(1986,0,{'11,500 km^2';'Apr to Nov 1986';'\it{Ferringo & Gould, 1987}'},'vert','bot','horiz','right','fontsize',6,'color',col(4,:))
+
+% Amery 10,000 km^2 in 1963 or 1964 (Fricker2002iceberg)
+ft = polyfit(year(1:end-3),A_calving(1:end-3,10),1);
+tmp = A_calving(:,10)-(A_calving(1,10)-ft(1)*34.25)-10; % 10 thousand km^2 event 34.25 years before 1997 
+%plot(year,tmp,'.-','color',col(5,:),'linew',1,'markersize',10)
+
+yr3 = [year(1:22) 2019.73 year(23:end)]; 
+tmp3 = [tmp(1:22)' tmp(22)+ft(1)*.5 tmp(23:end)']; 
+plot(yr3,tmp3,'.-','color',col(5,:),'linew',1,'markersize',10)
+tmp3 = polyval(ft,extrapyr); 
+plot(extrapyr,tmp3-tmp3(1)+tmp(end-1),':','color',col(5,:),'linew',1)
+text(year(ind),tmp(ind),'Amery','horiz','left','vert','top','fontsize',8,'color',col(5,:))
+plot([1963.5 1963.55 year(1)],[0 -10 tmp(1)],':.','color',col(5,:),'linew',1,'markersize',10)
+text(1963.5,0,{'10,000 km^2';'late 1963 or early 1964';'\it{Fricker et al., 2002}'},'vert','bot','horiz','left','fontsize',6,'color',col(5,:))
+text(2019.75,0,{'Iceberg D-28';'Sept 25, 2019';'\it{Walker et al., 2021}'},'vert','bot','horiz','left','fontsize',6,'color',col(5,:))
+
+box off 
+axis tight
+hyline = yline(0,'color',rgb('gray')); 
+uistack(hyline,'bottom') 
+ylabel 'area wrt maximum known extents (thousands of km^2)'
+
+axis([1962 max(extrapyr) -18 1.9])
+% export_fig Ross_FRIS_calving.png -r500 -p0.01 -painters
+% export_fig Ross_FRIS_calving_annotated.png -r500 -p0.01 -painters
+
+%%
+
+rng(1) % seed the random number generator for repeatable colors 
+col = rand(181,3); 
+
+col = double(lab2rgb([80*rand(181,1)+5 128*2*(rand(181,1)-0.5) 128*2*(rand(181,1)-0.5)],'OutputType','uint8'))/255;
+
+A = A_calving-A_calving(1,:);
 
 figure
-plot(year,A_calving(:,132))
+h = plot(year,A(:,1:181)); 
+for k = 1:181
+   h(k).Color = col(k,:); 
+   if k==132   
+      txt(k)=text(year(end),A(end,k),[' ',D.name{k}],'fontsize',6,'color',col(k,:),'vert','middle');
+   else
+      txt(k)=text(year(end-1),A(end-1,k),[' ',D.name{k}],'fontsize',6,'color',col(k,:),'vert','middle');
+   end
+end
+
+% Fix a couple of labels: 
+%txt(10).VerticalAlignment = 'top'; % amery
+txt(22).VerticalAlignment = 'bottom'; % brunt
+txt(86).VerticalAlignment = 'top'; % larsen C
+
+
 hold on
-polyfit(year(5:end),A_calving(5:end,132),1)
-ft = polyfit(year(5:end),A_calving(5:end,132),1)
-plot(2005:2100,polyval(ft,2005:2100))
-yline(A_calving(1,132))
-
-figure
-plot(year,M_calving(:,132))
-hold on
-ftm = polyfit(year(5:end),M_calving(5:end,132),1)
-plot(2005:2100,polyval(ftm,2005:2100))
-yline(M_calving(1,132))
-title mass
-
-
+plot(year,A(:,end),'k-','linewidth',2) 
+text(year(end),A(end,end),' Antarctica','fontsize',6,'fontweight','bold')
+axis tight
+box off
+ylabel 'area change (1000\timeskm^2)'
--- icecube2calvingflux.m
+++ icecube2calvingflux.m
@@ -9,7 +9,7 @@
 %% Load data: 
 
 load icemask_composite.mat
-load('issm_calving_melt_setup.mat','ground')
+load('/Users/cgreene/Documents/MATLAB/DEM_generation/issm_calving_melt_setup.mat','ground')
 
 fn = '/Users/cgreene/Documents/MATLAB/DEM_generation/extruded_antarctica_2021-10-18.h5'; 
 mask = permute(h5read(fn,'/iceshelf_mask'),[2 1]); 
@@ -22,6 +22,30 @@ rho_ice = 917;
 D = load('iceshelves_2008_v2.mat');
 tidal = ismember(bedmachine_interp('mask',X,Y),[0 3]) & ~ground; 
 
+%% Error fields  
+
+v_source = permute(h5read(fn,'/v_source'),[2 1]); 
+
+vx_err = itslive_data('vx_err'); 
+vy_err = itslive_data('vy_err'); 
+
+% Use Measures v2 errors wherever it was the velocity source:
+filename = 'antarctica_ice_velocity_450m_v2.nc'; 
+xm = -2800000:450:2800000; 
+ym = (-2800000:450:2800000)'; 
+vx_err(v_source==2) = interp2(xm,ym,double(rot90(ncread(filename,'ERRX'))),X(v_source==2),Y(v_source==2)); 
+vy_err(v_source==2) = interp2(xm,ym,double(rot90(ncread(filename,'ERRY'))),X(v_source==2),Y(v_source==2)); 
+
+% Extend, but downsample first to make the inpainting tractable 
+skip = 4; 
+vx_err_r = vx_err(1:skip:end,1:skip:end); 
+vy_err_r = vy_err(1:skip:end,1:skip:end); 
+vx_err_r = regionfill(vx_err_r,isnan(vx_err_r)); 
+vy_err_r = regionfill(vy_err_r,isnan(vy_err_r)); 
+vx_err(isnan(vx_err)) = interp2(x(1:skip:end),y(1:skip:end),vx_err_r,X(isnan(vx_err)),Y(isnan(vy_err)));
+vy_err(isnan(vy_err)) = interp2(x(1:skip:end),y(1:skip:end),vy_err_r,X(isnan(vy_err)),Y(isnan(vy_err)));
+
+clear v_source filename xm ym vx_err_r vy_err_r skip
 %%
 
 always_ice = imfill(all(ice,3),8,'holes'); 
@@ -32,6 +56,7 @@ action_ice = ever_ice & ~always_ice; % the grid cells where anything ever change
 [Lat,~] = ps2ll(X,Y); 
 A = (diff(x(1:2)) ./ psdistortion(Lat) ).^2; % grid cell area, (accounting for polar stereographic distortion).  
 IceMass = rho_ice.*H.*A.*1e-12; % ice mass in each grid cell (Gt)
+IceMass_plus = rho_ice.*(H+10).*A.*1e-12; % Thickness is primarily a hydrostatic inversion applied to REMA. REMA errors are not explicitly given for the mosaic, but the Cryosphere mansucript describing REMA says surface elevation errors are "less than 1 m" and the pdf description of the accuracy of the strips says errors average around 0.6 m. Hydrostatic inversion applied to 0.6 would be 5.8 m thickness uncertainty, however thickness uncertainty is also affected by FAC uncertainty. Now we're into uncertainties of uncertainties and how they compound together and we could put a great deal of effort into getting an exact measure of uncertainty, but of course even that would all be made up. Thus, let's just say there's 10 m thickness uncertainty everywhere and leave it at that.  
 clear Lat
 
 %% Position of grid cells after one year
@@ -44,7 +69,6 @@ clear Lat
 
 X1 = X; 
 Y1 = Y; 
-
 for k = 1:10
    dx = interp2(x,y,vx,X1,Y1); 
    dy = interp2(x,y,vy,X1,Y1); 
@@ -54,34 +78,68 @@ for k = 1:10
    k
 end
 
+% Do it again with velocity error: 
+X1_plus = X; 
+Y1_plus = Y; 
+for k = 1:10
+   dx = interp2(x,y,(abs(vx)+vx_err).*sign(vx),X1_plus,Y1_plus); 
+   dy = interp2(x,y,(abs(vy)+vy_err).*sign(vy),X1_plus,Y1_plus); 
+   
+   X1_plus = X1_plus+dx/10; 
+   Y1_plus = Y1_plus+dy/10; 
+   k
+end
+
+clear dx dy 
 %% Steady-state mass rate: 
 
 % A grid of steady-state mass rate at the calving front: 
 Mdot_cf = (double(always_ice) - interp2(x,y,double(always_ice),X1,Y1)) .* IceMass; 
 Mdot_cf(isnan(Mdot_cf)) = 0; % bc we count it as zero anyway, and now we don't have to deal with nans. 
 
+% Error estimates: Note these are the multiplied effects of velocity and thickness error. Technically, vel and thicness errors are independent and their effects should be root-sum-squared, so these numbers may err on the side of being higher than formal errors.  
+Mdot_cf_plus = (double(always_ice) - interp2(x,y,double(always_ice),X1_plus,Y1_plus)) .* IceMass_plus; 
+Mdot_cf_plus(isnan(Mdot_cf_plus)) = 0; 
+
 % A grid of steady-state mass rate at the calving front: 
 Mdot_gl = (interp2(x,y,double(tidal),X1,Y1) - double(tidal)) .* IceMass; 
 Mdot_gl(isnan(Mdot_gl)) = 0; 
 
+Mdot_gl_plus = (interp2(x,y,double(tidal),X1_plus,Y1_plus) - double(tidal)) .* IceMass_plus; 
+Mdot_gl_plus(isnan(Mdot_gl_plus)) = 0; 
+
 % Convert the grid to 2D for quick summing: 
 Mdot_cf2 = cube2rect(Mdot_cf,ever_ice); 
+Mdot_cf2_plus = cube2rect(Mdot_cf_plus,ever_ice); 
 Mdot_gl2 = cube2rect(Mdot_gl,ever_ice); 
+Mdot_gl2_plus = cube2rect(Mdot_gl_plus,ever_ice); 
 mask2 = cube2rect(mask,ever_ice); 
 
+% Calculte the calving flux of each ice shelf: 
 CF_ss = nan(183,1); 
+CF_ss_plus = nan(183,1); 
 GL_ss = nan(183,1); 
+GL_ss_plus = nan(183,1); 
 for k = 1:181
    CF_ss(k) = sum(Mdot_cf2(:,mask2==k)); 
+   CF_ss_plus(k) = sum(Mdot_cf2_plus(:,mask2==k)); 
    GL_ss(k) = sum(Mdot_gl2(:,mask2==k)); 
+   GL_ss_plus(k) = sum(Mdot_gl2_plus(:,mask2==k)); 
 end
 CF_ss(182) = sum(Mdot_cf2(:,mask2==0)); 
 CF_ss(183) = sum(Mdot_cf2);
-CF_ss(183)
+CF_ss_plus(182) = sum(Mdot_cf2_plus(:,mask2==0)); 
+CF_ss_plus(183) = sum(Mdot_cf2_plus);
 
 GL_ss(182) = sum(Mdot_gl2(:,mask2==0)); 
 GL_ss(183) = sum(Mdot_gl2);
-GL_ss(183)
+GL_ss_plus(182) = sum(Mdot_gl2_plus(:,mask2==0)); 
+GL_ss_plus(183) = sum(Mdot_gl2_plus);
+
+% Calculate error estimates as "plus" estimates minus 
+CF_ss_err = CF_ss_plus - CF_ss; 
+GL_ss_err = GL_ss_plus - GL_ss; 
+
 
 figure
 bar((1:182)-.5,CF_ss(1:182))
@@ -125,21 +183,29 @@ axis tight off
 
 ice2 = cube2rect(ice,tidal & ever_ice); 
 IceMass2 = cube2rect(IceMass,tidal & ever_ice); 
+IceMass2_plus = cube2rect(IceMass_plus,tidal & ever_ice); 
 mask2 = cube2rect(mask,tidal & ever_ice); 
 
 M2 = double(ice2).*IceMass2;
+M2_plus = double(ice2).*IceMass2_plus;
 A2 = double(ice2).*cube2rect(A,tidal & ever_ice);
 
 M_calving = nan(24,183); 
+M_calving_plus = nan(24,183); 
 A_calving = nan(24,183); 
 for k = 1:181
    M_calving(:,k) = sum(M2(:,mask2==k),2); 
+   M_calving_plus(:,k) = sum(M2_plus(:,mask2==k),2); 
    A_calving(:,k) = sum(A2(:,mask2==k),2); 
 end
 M_calving(:,182) = sum(M2(:,mask2==0),2); 
 M_calving(:,183) = sum(M2,2); 
+M_calving_plus(:,182) = sum(M2_plus(:,mask2==0),2); 
+M_calving_plus(:,183) = sum(M2_plus,2); 
 A_calving(:,182) = sum(A2(:,mask2==0),2); 
 A_calving(:,183) = sum(A2,2); 
+
+M_calving_err = M_calving_plus - M_calving; 
 
 dM = diff(M_calving); 
 
@@ -154,7 +220,7 @@ yline(CF_ss(end))
 ylabel 'calving rate (Gt/yr)' 
 
 readme = 'ice area time series and corresponding mass time series.  Created by icecube2calvingflux.m';   
-% save('calving_flux_timeseries.mat','year','readme','CF_ss','GL_ss','A_calving','M_calving')
+% save('/Users/cgreene/Documents/GitHub/ice-shelf-geometry/calving_flux_timeseries.mat','year','readme','CF_ss','GL_ss','A_calving','M_calving','CF_ss_err','GL_ss_err','M_calving_err')
 
 %% 
 % 
